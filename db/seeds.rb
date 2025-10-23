2a333b8db04145123c45e91fcd5216ed1a5c161f
jean toneli <jeantoneli@gmail.com>
2025-10-23 20:18:52 +0100

    adding fake flats

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,23 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+require 'faker'
+
+puts "🧹 Clearing existing data..."
+Flat.destroy_all
+User.destroy_all
+
+puts "👤 Creating a fake user..."
+user = User.create!(email: "test@example.com", password: "password")
+
+puts "🏠 Seeding fake flats..."
+30.times do
+  Flat.create!(
+    location: Faker::Address.city,
+    description: Faker::Lorem.paragraph(sentence_count: 3),
+    photo: Faker::LoremFlickr.image(size: "600x400", search_terms: ['apartment', 'flat']),
+    user: user
+  )
+end
+
+puts "✅ Done! Created #{Flat.count} fake flats for user #{user.email}."
